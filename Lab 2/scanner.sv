57d6faaecee7f15b549708a4d8e4075474e4dcf7
Isabella Vera <97315103+isabella-vera@users.noreply.github.com>
2026-09-15 17:46:29 -0700

    Update scanner module for row selection logic
    Refactor scanner module to use count comparisons for row selection.

--- Lab 2/scanner.sv
+++ Lab 2/scanner.sv
@@ -1,27 +1,26 @@
-// Isabella Vera
-// ivera@g.hmc.edu
-// Created 9/11/2026
-// Toggles between 4 states at 2 Hz to scan 
-
 module scanner(input logic clk,
-			   input logic reset,
-			   input logic enable,
-			   
-			   output logic [3:0] rows);
-			   
-			   logic clkEdge;
-			   logic [1:0] count;
-			   
-	counter #(23, 6000000, 2) rowSelect(clk, reset, enable, count);
-			   
-			   always_comb begin
-				case (count)
-					2'b00  : rows = 4'b1000;
-					2'b01  : rows = 4'b0100;
-					2'b10  : rows = 4'b0010;
-					2'b11  : rows = 4'b0001;
-					default: rows = 4'b0000;
-				endcase
-			   end
+							 input logic reset,
+							 input logic enable,
+
+							 output logic [3:0] rows);
+
+    logic [22:0] count;
+
+    counter #(23, 6000000) rowSelect(clk, reset, enable, count);
+
+    always_comb begin
+        if (count < 6000000/4) begin
+            rows = 4'b1000;
+        end
+        else if (count < 6000000/2) begin
+            rows = 4'b0100;
+        end
+        else if (count < 3*6000000/4) begin
+            rows = 4'b0010;
+        end
+        else begin
+            rows = 4'b0001;
+        end
+    end
 
 endmodule
